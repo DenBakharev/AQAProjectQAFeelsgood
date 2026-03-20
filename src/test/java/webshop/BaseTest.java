98832295a58045a117700704b23c80fe2515c4e3
DenBakharev <java.bakharev@gmail.com>
2026-03-20 13:40:24 +0300

    Урок по Удаленному запуску

--- src/test/java/webshop/BaseTest.java
+++ src/test/java/webshop/BaseTest.java
@@ -5,27 +5,36 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import webshop.config.WebDriverConfig;
 import webshop.util.AttachManager;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.hasWebDriverStarted;
+import static webshop.config.Config.getSelenoidChromeOptions;
+import static webshop.config.Config.getWebDriverConfig;
 
 public class BaseTest {
+
+    private static final WebDriverConfig config = getWebDriverConfig();
+
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browserSize = config.browserSize();
+        Configuration.browser = config.browser();
+
+//        Configuration.headless = true;
+        if ("remote".equals(config.run())) {
+            Configuration.remote =
+                    "https://" + config.selenoidUser() + ":" + config.selenoidPassword() + "@" + config.selenoidUrl();
+            Configuration.browserCapabilities = getSelenoidChromeOptions();
+        }
+
+//        Configuration.remote = "http://localhost:4444/wd/hub";
+//        Configuration.browserCapabilities.setCapability("enableVNC", true);
     }
 
-    @BeforeAll
-    static void before() {
-        Configuration.browserSize = "1920x1080";
-    }
-
-    //    @BeforeEach
-//    void closeBrowser() {
-//        closeWebDriver();
-//    }
     @AfterEach
     void after() {
 
@@ -33,6 +42,9 @@ public class BaseTest {
             AttachManager.takeScreenshot();
             AttachManager.pageSource();
             AttachManager.browserConsoleLogs();
+            if ("remote".equals(config.run())) {
+                AttachManager.addVideo();
+            }
 
             clearBrowserLocalStorage();
             closeWebDriver();
--- src/test/java/webshop/config/Config.java
+++ src/test/java/webshop/config/Config.java
@@ -1,7 +1,35 @@
 package webshop.config;
 
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Config {
     public static final String WEB_SHOP_URL = "https://demowebshop.tricentis.com";
     public static final String WEB_SHOP_REGISTRATION_URL = WEB_SHOP_URL + "/register";
     public static final String WEB_SHOP_LOGIN_URL = WEB_SHOP_URL + "/login";
+
+    public static WebDriverConfig getWebDriverConfig() {
+        return ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+    }
+
+    public static ChromeOptions getSelenoidChromeOptions() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("browserVersion", "128");
+
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("name", "useToastTest");            // имя сессии
+        selenoidOptions.put("sessionTimeout", "15m");           // таймаут сессии
+        selenoidOptions.put("env", List.of("TZ=UTC"));          // временная зона
+        selenoidOptions.put("labels", Map.of("manual", "true")); // кнопка ручного удаления
+        selenoidOptions.put("enableVideo", true);               // запись видео
+        selenoidOptions.put("enableVNC", true);
+        options.setCapability("selenoid:options", selenoidOptions);
+
+        return options;
+    }
 }
